8c149cc506508a2ed19f7c4401f7facf341238a6
E.J. Bevenour <ejbevenour@live.com>
2013-08-23 01:49:06 -0400

    fix too many arguments on start

--- autorip.sh
+++ autorip.sh
@@ -8,16 +8,17 @@ else
     echo "Autorip comes with ABSOLUTELY NO WARRANTY; for details type `show w'."
     echo "This is free software, and you are welcome to redistribute it"
     echo "under certain conditions; type `show c' for details."
+    echo ""
     echo "Error: makemkvcon is not installed."
     echo "Please install it or contact your system administrator."
     exit 1
 fi
 
-if [ $# != 1 ]
-then
-    echo "Error: too many arguments."
-    exit 1
-fi
+#if [ $# != 1 ]
+#then
+#    echo "Error: too many arguments."
+#    exit 1
+#fi
 
 clear
     echo "Autorip version .1, Copyright (C) 2013 E. Bevenour" 
