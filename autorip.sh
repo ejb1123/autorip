19e21604e1e4a5455e45e7b57bea1f2fe4e24556
E.J. Bevenour <ej@ejb1123.tk>
2013-08-23 21:16:38 -0400

    better disc detection and Saves file as movie name

--- autorip.sh
+++ autorip.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+shopt -s nullglob
 if [ -x /usr/bin/makemkvcon ]
 then
-echo "makemkvcon found."
+	echo "makemkvcon found."
 else
-    echo "Error: makemkvcon is not installed."
-    echo "Please install it or contact your system administrator."
-    exit 1
+	echo "Error: makemkvcon is not installed."
+	echo "Please install it or contact your system administrator."
+exit 1
 fi
 
 #if [ $# != 1 ]
@@ -17,18 +17,27 @@ fi
 
 clear
 dir1=""
-member=VolumeChanged
 echo "Welcome to autorip."
 echo "Directory to save to: "
-
 read dir1
-#while [ 1 == 1 ]
-#do
-dbus-monitor --profile "member='$member'" |
-while read -r line; 
-do
-    echo "mounted"
-    vol1=`volname /dev/sr0`
-    mkdir $dir1/$vol1
-    makemkvcon --minlength=1200 mkv disc:0 all $dir1/$vol1
+while [ 1 == 1 ];
+	do
+	vol1=`volname /dev/sr0 2>&1 | xargs`
+	if [[ $vol1 != "volname: No medium found" ]]
+	then
+		echo "Mounted."
+		mkdir -p $dir1/$vol1
+		makemkvcon --minlength=1200 mkv disc:0 all $dir1/$vol1
+		dir2="$dir1/$vol1/title.mkv"
+		if [[ -e $dir1/$vol1/title00.mkv ]];
+		then
+			mv $dir1/$vol1/title00.mkv $dir1/$vol1/$vol1.mkv
+		fi
+	sudo umount /dev/sr0
+	sudo eject /dev/sr0
+	sleep 10
+	else
+		echo "No disc mounted."
+		sleep 10
+	fi
 done
