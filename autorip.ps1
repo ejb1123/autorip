bfe8c77bfa923867408f60b75d6a90682382f2d6
unknown <Administrator@server1.ejb1123.tk>
2013-08-26 13:45:41 -0400

    fixed script

--- autorip.ps1
+++ autorip.ps1
@@ -10,15 +10,16 @@
             $makemkvpath = Read-Host 'What is your makemkvcon.exe folder path'
         }
         echo "Welcome to autorip."
-        $dir1 = Read-Host 'Directory to save to: '
-        $deviceId = Read-Host 'What is your disc drive letter (eg. D:): '
+        $dir1 = Read-Host 'Directory to save to'
+        $device = Read-Host 'What is your disc drive letter'
+        $semicolon = ":"
         
         try 
         {
             while (1-eq 1)
             {
                 # Grab WMI object
-            $w = Get-WmiObject -Class Win32_LogicalDisk -Filter "DriveType = 5 and DeviceID = 'D:' " -errorvariable MyErr -erroraction Stop;       
+            $w = Get-WmiObject -Class Win32_LogicalDisk -Filter "DriveType = 5 and DeviceID = '$device$semicolon' " -errorvariable MyErr -erroraction Stop;       
                 $w | ForEach-Object {
                     # Populate the properties
                     $deviceId = $_.DeviceID
@@ -46,6 +47,7 @@
                         finally
                         {
                         }
+                        Show-BalloonTip -Title “Autorip” -MessageType Info -Message “$volumeName is ripped” -Duration 10000
                         ejectcd
                     }
                }
@@ -66,6 +68,30 @@
             echo $err
         }
     }
+    function Show-BalloonTip {            
+[cmdletbinding()]            
+param(            
+ [parameter(Mandatory=$true)]            
+ [string]$Title,            
+ [ValidateSet("Info","Warning","Error")]             
+ [string]$MessageType = "Info",            
+ [parameter(Mandatory=$true)]            
+ [string]$Message,            
+ [string]$Duration=10000            
+)            
+
+[system.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms') | Out-Null            
+$balloon = New-Object System.Windows.Forms.NotifyIcon            
+$path = Get-Process -id $pid | Select-Object -ExpandProperty Path            
+$icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)            
+$balloon.Icon = $icon            
+$balloon.BalloonTipIcon = $MessageType            
+$balloon.BalloonTipText = $Message            
+$balloon.BalloonTipTitle = $Title            
+$balloon.Visible = $true            
+$balloon.ShowBalloonTip($Duration)            
+
+}
 
  
 # Execute this function
